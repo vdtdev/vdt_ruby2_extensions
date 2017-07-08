d7a082dd36cd8f49ed09015ac986527ae9701520
Wade H <vdtdev@gmail.com>
2017-07-08 16:53:59 -0400

    Updated `match_all` method in `MatchData` with optional parameter to indicate group names should be converted to symbols in output hash

--- matchdata_extensions.rb
+++ matchdata_extensions.rb
@@ -1,11 +1,18 @@
 class MatchData
   ##
   # Produces a hash of capture groups
+  # @param [Hash] options Optional parameter hash
+  # @option options [Boolean] :symbols If true, convert group names to symbols in output; default false
   # @return {Hash} Hash using group names as keys and captures as values
   #
-  def named_captures()
+  def named_captures(options={})
+    force_symbolic_names = options.fetch(:symbols, false)
     names = self.names
     capts = self.captures
+
+    # Convert names to symbols if indicated
+    names = names.map { |name| name.to_sym } if force_symbolic_names
+
     Hash[*names.zip(capts).flatten]
   end
 end
